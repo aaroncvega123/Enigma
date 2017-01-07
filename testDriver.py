13625ef5facf4f3888c552b51b25e95e80ce7798
aaroncvega123 <aaroncvega123@gmail.com>
2017-01-06 17:49:04 -0800

    Updated testDriver.py

--- testDriver.py
+++ testDriver.py
@@ -6,9 +6,12 @@ inheritance issue in Wheel.py
 from WheelSet import WheelSet
 
 
-newSet = WheelSet("12345679")
+newSet = WheelSet("123456789")
 message = newSet.encryptString("Hello world")
 print(message)
+
+#Reset newSet to original state by creating new instance
+newSet = WheelSet("123456789")
 message = newSet.encryptString(message)
 
 #Supposed to read "Hello world."
